03340474fc0422490da8cff312831f1e059c9d23
LiAuTraver <me@liautraver.dev>
2024-11-05 20:48:56 +0800

    remove dups

--- projects/treev/treev.c
+++ projects/treev/treev.c
@@ -6,30 +6,9 @@
 
 #include "treev.h"
 
-void preorder_traversal(const Node *node) {
-	if (node == NULL) {
-		return;
-	}
-	fprintf(stdout, "%c", node->data);
-	preorder_traversal(node->left);
-	preorder_traversal(node->right);
-}
-
-
-int main() {
-	println("Please input a string:");
-	fflush(stdout);
-	val chars = get_rec(Any.char_ptr_type, 100);
-	trim_str(chars);
-	println("Your input was:");
-	fprintf(stdout, "%s\n", chars);
-	fflush(stdout);
-	println("Make your string as a binary tree:");
-	smart var nodes = alloc(Node, strlen(chars));
-	var				dep		= 0ll;
-	build_tree(&nodes, &chars, &dep);
-	println("Depth:");
-	println(dep);
-	println("Preorder traversal:");
-	preorder_traversal(&nodes[0]);
+int main(){
+	// get string and build a tree
+	// > root
+	// > child 1, child 2 ...
+	// > child 1.1, child 1.2 ...
 }
